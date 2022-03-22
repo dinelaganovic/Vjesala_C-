2005c0fb94fd03c4ff69745e01e5e41674df4885
Dinela <59210189+dinelaganovic@users.noreply.github.com>
2022-03-22 20:40:44 +0100

    Add files via upload

---
+++ IgraVesalaS/IgraVesalaS/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace IgraVesalaS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rec = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Coral;
+            this.panel1.BackgroundImage = global::IgraVesalaS.Properties.Resources.Slika1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(21, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1166, 806);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(1355, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Igraj";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1329, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Unesite jedno slovo:";
+            // 
+            // rec
+            // 
+            this.rec.Location = new System.Drawing.Point(1332, 284);
+            this.rec.Name = "rec";
+            this.rec.Size = new System.Drawing.Size(134, 22);
+            this.rec.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1342, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1512, 848);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox rec;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ IgraVesalaS/IgraVesalaS/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IgraVesalaS
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        string word;
+        string[] solution;
+        char guess;
+        string stringSolution;
+        int badguess = 0;
+        int goodguess = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            word = "ovesajse";
+            solution = new string[word.Length];
+            MakeSolutionStrg();
+        }
+
+        public void MakeSolutionStrg()
+        {
+            string temp = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                solution[i] = "_" + " ";
+                stringSolution = temp + solution[i];
+                temp = stringSolution;
+            }
+            label2.Text = stringSolution;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            bool trazi = false;
+            guess = rec.Text[0];
+            string temp = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i].CompareTo(guess) == 0)
+                {
+                    solution[i] = guess.ToString();
+                    goodguess++;
+                    if (goodguess == word.Length)
+                    {
+                       MessageBox.Show( "Pobedili ste!");
+                        
+                    }
+                    trazi = true;
+                }
+                stringSolution = temp + solution[i];
+                temp = stringSolution;
+            }
+            if (trazi == false)
+            {
+                badguess++;
+                if (badguess == 1)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap1 = new Bitmap(Properties.Resources.Glava);
+                    g.DrawImage(bitmap1, 465, 120, 330, 99);
+
+                }
+                if (badguess == 2)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap2 = new Bitmap(Properties.Resources.Telo);
+                    g.DrawImage(bitmap2, 475, 195, 297, 234);
+                }
+                if (badguess == 3)
+                {
+                    Graphics g2 = panel1.CreateGraphics();
+                    Bitmap bitmap3 = new Bitmap(Properties.Resources.NogaLeva);
+                    g2.DrawImage(bitmap3, 448, 355, 307, 192);
+                }
+                if (badguess == 4)
+                {
+                    Graphics g3 = panel1.CreateGraphics();
+                    Bitmap bitmap4 = new Bitmap(Properties.Resources.DNoga);
+                    g3.DrawImage(bitmap4, 626, 348, 127, 185);
+                }
+                if (badguess == 5)
+                {
+                    Graphics g4 = panel1.CreateGraphics();
+                    Bitmap bitmap5 = new Bitmap(Properties.Resources.RukaLeva);
+                    g4.DrawImage(bitmap5, 390, 225, 227, 122);
+                }
+                if (badguess == 6)
+                {
+                    Graphics g5 = panel1.CreateGraphics();
+                    Bitmap bitmap6 = new Bitmap(Properties.Resources.RukaDesna);
+                    g5.DrawImage(bitmap6, 628, 226, 180, 122);
+                    MessageBox.Show( "Izgubili ste!");
+                  
+                }
+            }
+            label2.Text = stringSolution;
+        }      
+    }
+}
